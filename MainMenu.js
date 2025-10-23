7d16154386c4a2719fe1541878f5db8be45b20d6
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-10-23 17:35:07 +0100

    Add ModeSelector to MainMenu for improved game mode selection

--- MainMenu.js
+++ MainMenu.js
@@ -1,15 +1,23 @@
 import MenuHeader from './MenuHeader.js';
 import PlayGame from './PlayGame.js';
+import ModeSelector from './ModeSelector.js';
 
 export default class MainMenu {
   constructor() {
     // Create element to contain sections of main menu
     this.element = document.createElement('div');
 
+    // Initialise the parts of the main menu
     this.menuHeader = new MenuHeader();
     this.playGame = new PlayGame();
+    this.modeSelector = new ModeSelector('standard', 'classic');
 
-    this.element.append(this.menuHeader.element, this.playGame.element);
+    // Display the elements of each part
+    this.element.append(
+      this.menuHeader.element,
+      this.playGame.element,
+      this.modeSelector.element,
+    );
 
     // Render the main menu
     document.getElementById('content').replaceChildren(this.element);
