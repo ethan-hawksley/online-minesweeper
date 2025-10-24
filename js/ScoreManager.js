7243d46a59102abc362f18812680b1947e91ff31
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-10-24 15:02:58 +0100

    Add Streak integration to ScoreManager for enhanced score tracking

--- js/ScoreManager.js
+++ js/ScoreManager.js
@@ -1,4 +1,5 @@
 import Score from './Score.js';
+import Streak from './Streak.js';
 
 export default class ScoreManager {
   constructor(databaseService, mode, difficulty, modeSettings) {
@@ -7,16 +8,21 @@ export default class ScoreManager {
 
     // Initialise the score so that the player's score can be changed
     this.score = new Score(databaseService, mode, difficulty, modeSettings);
+    this.streak = new Streak(databaseService);
 
     // Show the score class on the page
-    this.element.append(this.score.element);
+    this.element.append(this.score.element, this.streak.element);
   }
 
   gameWon() {
+    // Relay the game is won
     this.score.gameWon();
+    this.streak.gameWon();
   }
 
   gameOver() {
+    // Relay the game is over
     this.score.gameOver();
+    this.streak.gameOver();
   }
 }
