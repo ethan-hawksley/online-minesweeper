2a10526295778e84922e1712927c4c363cddcb3a
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-10-24 12:22:08 +0100

    Refactor ScoreManager to integrate DatabaseService into Score initialization

--- js/ScoreManager.js
+++ js/ScoreManager.js
@@ -1,12 +1,12 @@
 import Score from './Score.js';
 
 export default class ScoreManager {
-  constructor(mode, difficulty, modeSettings) {
+  constructor(databaseService, mode, difficulty, modeSettings) {
     // Create element to encapsulate the contents of the class
     this.element = document.createElement('div');
 
     // Initialise the score so that the player's score can be changed
-    this.score = new Score(mode, difficulty, modeSettings);
+    this.score = new Score(databaseService, mode, difficulty, modeSettings);
 
     // Show the score class on the page
     this.element.append(this.score.element);
