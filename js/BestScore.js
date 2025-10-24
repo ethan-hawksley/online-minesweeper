06e7401529b016bbb55f2ecbc25bbe3406dc8870
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-10-24 13:21:02 +0100

    Add best score management for custom difficulty in BestScore class

--- js/BestScore.js
+++ js/BestScore.js
@@ -11,6 +11,7 @@ export default class BestScore {
     // Create element to display the best score to the player
     this.element = document.createElement('h2');
     this.element.textContent = 'Best Score: ';
+    this.element.hidden = this.difficulty === 'custom';
 
     // Fetch the best score from the database service
     this.showBestScore();
@@ -27,6 +28,48 @@ export default class BestScore {
       this.element.textContent = `Best Score: ${this.bestScore}`;
     } catch (e) {
       console.error('Error encountered whilst fetching best score', e);
+    }
+  }
+
+  checkBestScore(score) {
+    if (this.difficulty === 'custom') {
+      // Custom difficulties do not save best scores
+      return;
+    }
+    // Different modes either prioritise a high score or a low score
+    switch (this.mode) {
+      case 'classic':
+        // Classic mode - lower score the better
+        if (score < this.bestScore) {
+          this.setBestScore(score);
+        }
+        break;
+      case 'timeAttack':
+        // Time Attack mode - higher score the better
+        if (score > this.bestScore) {
+          this.setBestScore(score);
+        }
+        break;
+      default:
+        console.error('Invalid mode', this.mode);
+        break;
+    }
+  }
+
+  async setBestScore(score) {
+    try {
+      // Update the database's held best score value
+      await this.databaseService.updateBestScore(
+        this.mode,
+        this.difficulty,
+        score,
+      );
+      // Update held best score
+      this.bestScore = score;
+      // Render new best score
+      this.element.textContent = `Best Score: ${this.bestScore}`;
+    } catch (e) {
+      console.error('Error whilst setting best score', e);
     }
   }
 }
