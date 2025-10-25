504b45519b6afbaf4a01ddaff38a06540af73be4
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-10-25 14:13:32 +0100

    Add mute functionality to Settings with localStorage support

--- js/Settings.js
+++ js/Settings.js
@@ -3,6 +3,8 @@ export default class Settings {
     this.audioService = audioService;
     // Retrieve whether dark mode is set already
     this.darkMode = localStorage.getItem('darkMode') === 'true';
+    // Retrieve whether the game is muted or not
+    this.muted = localStorage.getItem('muted') === 'true';
 
     // Create element to encapsulate the HTML for the class
     this.element = document.createElement('div');
@@ -16,22 +18,43 @@ export default class Settings {
       this.toggleDarkMode();
     });
 
-    this.updateDarkModeUI();
+    this.muteButton = document.createElement('button');
+    this.muteButton.className = 'menu-button';
+    // Toggle the muted state whenever clicked
+    this.muteButton.addEventListener('click', () => {
+      this.toggleMute();
+      this.audioService.playAudio('button-click');
+    });
 
-    this.element.append(this.darkModeButton);
+    this.updateDarkMode();
+    this.updateMute();
+
+    this.element.append(this.darkModeButton, this.muteButton);
   }
 
   toggleDarkMode() {
     // Invert the dark mode state
     this.darkMode = !this.darkMode;
     localStorage.setItem('darkMode', this.darkMode.toString());
-    this.updateDarkModeUI();
+    this.updateDarkMode();
   }
 
-  updateDarkModeUI() {
+  updateDarkMode() {
     // Set the body to have the dark mode class if the game is in dark mode
     document.body.classList.toggle('dark-mode', this.darkMode);
     // Update the dark mode button so that it reflects the current state
     this.darkModeButton.textContent = `Dark Mode ${this.darkMode ? 'On' : 'Off'}`;
+  }
+
+  toggleMute() {
+    this.muted = !this.muted;
+    localStorage.setItem('muted', this.muted.toString());
+    this.updateMute();
+  }
+
+  updateMute() {
+    this.audioService.muted = this.muted;
+    // Update the mute button so it reflects the current state
+    this.muteButton.textContent = this.muted ? 'Muted' : 'Unmuted';
   }
 }
