9abb074d00c6a7a1e691d40f88fd28b355a142d2
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-10-25 13:00:29 +0100

    Add async splash text loading to MenuHeader

--- js/MenuHeader.js
+++ js/MenuHeader.js
@@ -5,7 +5,30 @@ export default class MenuHeader {
     // Create title for main menu
     const title = document.createElement('h1');
     title.textContent = 'MineDuo';
+    // Add empty splash text to be filled later
+    this.splashText = document.createElement('h2');
+    this.splashText.id = 'splash-text';
 
-    this.element.append(title);
+    this.element.append(title, this.splashText);
+
+    this.loadSplashTextAsync();
+  }
+
+  async loadSplashTextAsync() {
+    try {
+      // Load joke from external API
+      const response = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+      // Parse json
+      const responseData = await response.json();
+
+      // Display the joke on the main menu
+      this.splashText.textContent = responseData?.joke;
+    } catch (e) {
+      console.error('Error whilst fetching splash text', e);
+    }
   }
 }
